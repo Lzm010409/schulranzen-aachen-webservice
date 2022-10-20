5ec0e50d30ef659b0815757a2be5cd4f91384e17
start.vaadin.com <no-reply@vaadin.com>
2022-10-20 11:40:22 +0000

    Generated project

---
+++ src/main/java/lukegoll/schulranzen/aachen/webservices/views/kunden/KundenView.java
@@ -0,0 +1,31 @@
+package lukegoll.schulranzen.aachen.webservices.views.kunden;
+
+import com.vaadin.flow.component.html.H2;
+import com.vaadin.flow.component.html.Image;
+import com.vaadin.flow.component.html.Paragraph;
+import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+import com.vaadin.flow.router.PageTitle;
+import com.vaadin.flow.router.Route;
+import lukegoll.schulranzen.aachen.webservices.views.MainLayout;
+
+@PageTitle("Kunden")
+@Route(value = "kunden", layout = MainLayout.class)
+public class KundenView extends VerticalLayout {
+
+    public KundenView() {
+        setSpacing(false);
+
+        Image img = new Image("images/empty-plant.png", "placeholder plant");
+        img.setWidth("200px");
+        add(img);
+
+        add(new H2("This place intentionally left empty"));
+        add(new Paragraph("It’s a place where you can grow your own UI 🤗"));
+
+        setSizeFull();
+        setJustifyContentMode(JustifyContentMode.CENTER);
+        setDefaultHorizontalComponentAlignment(Alignment.CENTER);
+        getStyle().set("text-align", "center");
+    }
+
+}
